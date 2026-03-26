387bf19ab57ac275a22db553c827dd4134eb55ea
rokan45 <rokanuzzaman4567@gmail.com>
2026-03-26 23:27:40 +0600

    added badge at pricing card

--- src/components/Pricing/PricingCard.jsx
+++ src/components/Pricing/PricingCard.jsx
@@ -3,20 +3,20 @@ import Feature from './feature';
 
 const PricingCard = ({ pricing }) => {
     console.log(pricing)
-    const { name, price, currency, tagline, features, cta, highlighted } = pricing
+    const { name, price, currency, tagline, features, cta, highlighted,badge } = pricing
     return (
         <div className={`relative border p-6 rounded-xl flex flex-col gap-4 transition-all duration-300
             ${highlighted
                 ? 'border-blue-500 border-2 shadow-2xl scale-105 bg-white'
                 : 'border-gray-200 shadow-lg hover:shadow-md bg-white'
             }`}>
-            <h1 className='text-xl font-bold text-center'>{name}</h1>
+            <h1 className='text-xl font-bold text-center'>{name}{badge && <div className="badge badge-accent ml-2">Popular</div>}</h1>
             <div className='mt-2 text-center'>
                 <h2 className='text-2xl font-semibold'>{price}{currency}<span className='text-sm'>/mo</span></h2>
                 <h2 className='text-sm'>+3 mo free</h2>
             </div>
             <h2 className='p-3 bg-gray-100 rounded-md text-[12px] mt-4'>{tagline}</h2>
-            <div>
+            <div className='flex-1'>
                 {
                     features.map((feature, index) => <Feature key={index} feature={feature}></Feature>)
                 }
